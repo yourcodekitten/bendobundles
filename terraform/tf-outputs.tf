c45b731c7187a1bbb53e540d0af47f40ec60d8ba
code kitten <yourcodekitten@gmail.com>
2026-07-03 13:18:05 -0400

    feat(terraform): lambda function-name outputs (task-4 stated deliverable)

--- terraform/tf-outputs.tf
+++ terraform/tf-outputs.tf
@@ -1,3 +1,12 @@
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.this.name
 }
+
+# Handy for ops: `aws logs tail /aws/lambda/<name>`, manual invokes, etc.
+output "lambda_function_names" {
+  value = {
+    public_api  = module.lambda_public_api.lambda_function_name
+    admin_api   = module.lambda_admin_api.lambda_function_name
+    fulfillment = module.lambda_fulfillment.lambda_function_name
+  }
+}
